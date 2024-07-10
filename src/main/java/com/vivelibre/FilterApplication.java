727b977b91fb0fb4e56c075c9ca580d808673119
olitemGit <otemino@knowmadmood.com>
2024-07-10 08:48:20 +0200

    fix applications

--- src/main/java/com/vivelibre/FilterApplication.java
+++ src/main/java/com/vivelibre/FilterApplication.java
@@ -3,16 +3,11 @@ package com.vivelibre;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.vivelibre.filter.BookFilter;
-
-
 @SpringBootApplication
 public class FilterApplication {
 	
 	public static void main(String[] args) {
 		SpringApplication.run(FilterApplication.class, args);
-		BookFilter app = new BookFilter();
-	    app.filter("", null);
 	}
 
 }
